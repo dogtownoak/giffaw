43a9f9624977cfa2564f8cad1724146b10a219d6
dogtownoak <david.brandon.ford@gmail.com>
2018-11-20 09:29:56 -0800

    HTTPS to SSH

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,12 @@
-$(document).on("ready", function(){
+$(document).ready(function() {
+    console.log("Everything is ready, let's do this");
+})
 
-});
+var key = $('input').attr('value');
+
+var keyMine = "iR3W5Uukw68dMiz5ViAdDrNIMA0pOZwg";
+
+var giphyURL = "api.giphy.com";
+
+console.log(key);
+
